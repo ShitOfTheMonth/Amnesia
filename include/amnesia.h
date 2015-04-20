4a6a5471baa3a0925e7fc2930e7ecd494c9d78f4
Evan Darwin <evan@devil.io>
2015-04-20 04:14:36 -0700

    sockets?

--- include/amnesia.h
+++ include/amnesia.h
@@ -3,15 +3,26 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "amnesia_list.h"
 
-struct client_
+typedef struct _client amnesia_client;
+typedef struct _socket amnesia_socket;
+
+struct _client
 {
+  /* TODO */
 }
 
+struct _socket
+{
+  int sockfd
+}
+
+extern amnesia_socket amnesia_create_socket(void);
 
 #endif /* AMNESIA_H */
