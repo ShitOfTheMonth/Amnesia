39913e857d1a2874a23ff72a1417a3107489c482
Evan Darwin <evan@devil.io>
2015-04-20 20:33:04 -0700

    Build dammit

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@
 OUTPUT_PATH=./bin/amnesia
 SRC_PATH=./src
 
-gcc -Wall -c -Iinclude -o $OUTPUT_PATH $SRC_PATH/*
+gcc $SRC_PATH/* -Wall -c -Iinclude -o $OUTPUT_PATH
 
 echo "Compiled to $OUTPUT_PATH"
