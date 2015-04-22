19ea42bc9200e67838de2f0caee8dd0841b8b37b
Evan Darwin <evan@devil.io>
2015-04-21 18:35:48 -0700

    make.sh -> Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -Wall -o bin/amnesia -Iinclude src/*.c
--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-OUTPUT_PATH=./bin/amnesia
-SRC_PATH=./src
-
-gcc -Wall $SRC_PATH/*.c -Iinclude -o $OUTPUT_PATH
-
-echo "Compiled to $OUTPUT_PATH"
